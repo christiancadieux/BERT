e7520ac67e89bc560e0aeec27f77d4c59b818c78
Christian Cadieux <christian_cadieux2@comcast.com>
2023-06-25 17:27:45 -0600

    read

--- README.md
+++ README.md
@@ -15,37 +15,57 @@ Q=what is rdei
   Answer=a kubernetes control plane
   Model =a kubernetes control plane
   PASS 
+      SCORE      ANSWER
+      0.557109   a Kubernetes control plane 
+      0.137497   RDEI is a Kubernetes control plane 
 
 Q=who created rdei
   Answer=david arbuckle
   Model =david arbuckle
   PASS 
+      SCORE      ANSWER
+      0.812041   David Arbuckle 
+      0.068526   David Arbuckle at Concast 
 
 Q=when was rdei created
   Answer=2015
   Model =2015
   PASS 
+      SCORE      ANSWER
+      0.897995   2015. 
+      0.052985   2015. 
 
 Q=where was rdei created
   Answer=concast
   Model =concast
   PASS 
+      SCORE      ANSWER
+      0.554667   Concast 
+      0.225040   Concast in 2015. 
 
 Q=what is a rdei team
   Answer=grouping of users and kubernetes resources like namespaces
   Model =grouping of users and kubernetes resources
   PASS 
+      SCORE      ANSWER
+      0.273901   grouping of users and kubernetes resources 
+      0.030140   teams are grouping of users and kubernetes resources 
 
 Q=who owns the kubernetes resources
   Answer=the rdei team
   Model =the rdei team
   PASS 
+      SCORE      ANSWER
+      0.592871   The RDEI team 
+      0.157617   RDEI team 
 
 Q=who can access kubernetes resources
   Answer=the members of the team
   Model =the members of the team
   PASS 
+      SCORE      ANSWER
+      0.551723   the members of the team 
+      0.188457   members of the team 
 
 Tests: 7, PASSED: 7 
-
 ```
--- qa.py
+++ qa.py
@@ -4,6 +4,7 @@ import json
 
 from transformers import BertForQuestionAnswering
 from transformers import BertTokenizer
+from transformers import pipeline
 
 DATA="data.txt"
 ans="data-qa.json"
@@ -39,6 +40,9 @@ def process (question):
     sentence_embedding = encoding['token_type_ids']  #Segment embeddings
     tokens = tokenizer.convert_ids_to_tokens(inputs) #input tokens
 
+    nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+    scored_answers = nlp(question=question, context=paragraph, top_k=2)
+
     start_scores, end_scores = model(input_ids=torch.tensor([inputs]), token_type_ids=torch.tensor([sentence_embedding]), return_dict=False)
     start_index = torch.argmax(start_scores)
     end_index = torch.argmax(end_scores)
@@ -46,7 +50,7 @@ def process (question):
     answer = ' '.join(tokens[start_index:end_index+1])
 
     corrected_answer = correct_answer(answer)
-    return corrected_answer.strip()
+    return (corrected_answer.strip(), scored_answers)
 
 
 print ("Reading ", DATA)
@@ -62,14 +66,18 @@ count_test = 0
 count_pass = 0
 for qa in QAList:
      count_test += 1
-     model_ans = process(qa["q"])
+     (model_ans, scored_answers) = process(qa["q"])
 
      result="FAIL"
      if qa["a"].find(model_ans) >= 0 or model_ans.find(qa["a"]) >= 0:
         result="PASS"
         count_pass += 1
      
-     print ("Q=%s\n  Answer=%s\n  Model =%s\n  %s \n" % ( qa["q"], qa["a"] , model_ans, result))
+     print ("Q=%s\n  Answer=%s\n  Model =%s\n  %s " % ( qa["q"], qa["a"] , model_ans, result))
+     print ("      SCORE      ANSWER")
+     for ans in scored_answers:
+       print ("      %f   %s " % (ans['score'], ans['answer']))
+     print ()
 
 
 print  ("Tests: %d, PASSED: %d " % (count_test, count_pass))
